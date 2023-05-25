686c130596e5f9120d96c2cbd15d89baa1ac0fb8
Vi Minh Khôi <viminhkhoi2001@gmail.com>
2023-05-25 08:05:20 +0700

    a

--- src/pages/game/XSMB/0_History.jsx
+++ src/pages/game/XSMB/0_History.jsx
@@ -3,27 +3,33 @@ import { GetNameChoose } from "../../../funcUtils";
 import axios from "axios";
 
 const History = ({ isOpen, closePopup }) => {
-	const [historyGame, setHistoryGame] = useState(null);
-	function formatDate(m) {
-		new Date(m);
-		const dateString =
-			m.getUTCFullYear() +
-			"/" +
-			("0" + (m.getMonth() + 1)).slice(-2) +
-			"/" +
-			("0" + m.getDate()).slice(-2) +
-			"  " +
-			("0" + m.getHours()).slice(-2) +
-			":" +
-			("0" + m.getMinutes()).slice(-2);
-		return dateString;
-	}
-	axios
-		.get(`https://server.vnvip294.com/history/historyus`, {})
-		.then((res) => {
-			setHistoryGame(res.data.data);
-		})
-		.catch((err) => function () {});
+  const [historyGame, setHistoryGame] = useState(null);
+
+  useEffect(() => {
+    if (isOpen) {
+      axios
+        .get("https://server.vnvip294.com/history/historyus", {})
+        .then((res) => {
+          setHistoryGame(res.data.data);
+        })
+        .catch((err) => {});
+    }
+  }, [isOpen]);
+
+  function formatDate(m) {
+    new Date(m);
+    const dateString =
+      m.getUTCFullYear() +
+      "/" +
+      ("0" + (m.getMonth() + 1)).slice(-2) +
+      "/" +
+      ("0" + m.getDate()).slice(-2) +
+      "  " +
+      ("0" + m.getHours()).slice(-2) +
+      ":" +
+      ("0" + m.getMinutes()).slice(-2);
+    return dateString;
+  }
 	return (
 		<>
 			{isOpen && (
